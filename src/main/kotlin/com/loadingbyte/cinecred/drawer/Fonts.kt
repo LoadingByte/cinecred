61da4eccf27dba8550db65025ad5659eb764512d
LoadingByte <hello@loadingbyte.com>
2021-08-13 01:33:45 +0200

    Avoid creation of temp files when loading bundled fonts
    When the bundled fonts are loaded from a JAR, the creation of temporary
    files cannot be avoided.
    
    This commit also adds support for font collections in bundled fonts;
    however, this feature is not used yet by the current bundled fonts.

--- src/main/kotlin/com/loadingbyte/cinecred/drawer/Fonts.kt
+++ src/main/kotlin/com/loadingbyte/cinecred/drawer/Fonts.kt
@@ -11,17 +11,17 @@ import kotlin.io.path.listDirectoryEntries
 
 // Load the fonts that are bundled with this program.
 val BUNDLED_FONTS: List<Font> = run {
-    fun loadFonts(fontDir: Path) = fontDir
-        .listDirectoryEntries()
-        .map { file -> file.inputStream().use { Font.createFonts(it)[0] } }
-
     val uri = (object {}.javaClass).getResource("/fonts")!!.toURI()
     if (uri.scheme == "jar")
-        FileSystems
-            .newFileSystem(uri, emptyMap<String, Any>())
-            .use { fs -> loadFonts(fs.getPath("/fonts")) }
+        FileSystems.newFileSystem(uri, emptyMap<String, Any>()).use { fs ->
+            fs.getPath("/fonts").listDirectoryEntries().flatMap { file ->
+                file.inputStream().use { Font.createFonts(it).asList() }
+            }
+        }
     else
-        loadFonts(Path.of(uri))
+        Path.of(uri).listDirectoryEntries().flatMap { file ->
+            Font.createFonts(file.toFile()).asList()
+        }
 }
 
 // Load the fonts that are present on the system.

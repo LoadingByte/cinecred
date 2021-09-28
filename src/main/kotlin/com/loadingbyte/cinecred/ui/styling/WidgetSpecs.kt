12c664dd724a5de8a069d50ee2100eedcc15b685
LoadingByte <hello@loadingbyte.com>
2021-09-28 16:11:12 +0200

    Use timecode widgets for PageStyle frame settings

--- src/main/kotlin/com/loadingbyte/cinecred/ui/styling/WidgetSpecs.kt
+++ src/main/kotlin/com/loadingbyte/cinecred/ui/styling/WidgetSpecs.kt
@@ -30,7 +30,13 @@ private val GLOBAL_WIDGET_SPECS: List<StyleWidgetSpec<Global>> = listOf(
 
 
 private val PAGE_STYLE_WIDGET_SPECS: List<StyleWidgetSpec<PageStyle>> = listOf(
-    ToggleButtonGroupWidgetSpec(PageStyle::behavior.st(), LABEL)
+    ToggleButtonGroupWidgetSpec(PageStyle::behavior.st(), LABEL),
+    TimecodeWidgetSpec(
+        PageStyle::afterwardSlugFrames.st(), PageStyle::cardDurationFrames.st(),
+        PageStyle::cardFadeInFrames.st(), PageStyle::cardFadeOutFrames.st(),
+        getFPS = { _, styling, _ -> styling.global.fps },
+        getTimecodeFormat = { _, styling, _ -> styling.global.timecodeFormat }
+    )
 )
 
 

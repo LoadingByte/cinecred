43f14dd580870b3f23877306bd9ae0a4a2827c77
LoadingByte <hello@loadingbyte.com>
2024-09-27 16:22:53 +0200

    Harden the macOS menu l10n code and make it run on ARM

--- src/main/kotlin/com/loadingbyte/cinecred/ui/helper/MacOSMenuLocalizer.kt
+++ src/main/kotlin/com/loadingbyte/cinecred/ui/helper/MacOSMenuLocalizer.kt
@@ -6,7 +6,9 @@ import com.loadingbyte.cinecred.common.l10n
 import java.lang.foreign.*
 import java.lang.foreign.MemorySegment.NULL
 import java.lang.foreign.ValueLayout.ADDRESS
-import java.lang.foreign.ValueLayout.JAVA_INT
+import java.lang.foreign.ValueLayout.JAVA_LONG
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.MethodType.methodType
 
 
 /**
@@ -42,54 +44,97 @@ class MacOSMenuLocalizer {
     }
 
 
-    private fun getClass(name: String) =
-        Arena.ofConfined().use { arena -> objc_getClass.invokeExact(arena.allocateUtf8String(name)) as MemorySegment }
+    private fun notNull(ptr: MemorySegment, msg: String): MemorySegment = if (ptr != NULL) ptr else
+        throw NullPointerException("Unexpected null pointer returned by native call '$msg'")
 
-    private val objc_getClass = Linker.nativeLinker().downcallHandle(
-        Linker.nativeLinker().defaultLookup().find("objc_getClass").get(),
+
+    // Handle for objc_getClass():
+
+    private fun getClass(name: String): MemorySegment = Arena.ofConfined().use { arena ->
+        notNull(getClass.invokeExact(arena.allocateUtf8String(name)) as MemorySegment, "objc_getClass($name)")
+    }
+
+    private val getClass = Linker.nativeLinker().downcallHandle(
+        SymbolLookup.loaderLookup().find("objc_getClass").get(),
         FunctionDescriptor.of(ADDRESS, ADDRESS)
     )
 
 
-    private fun msgNil(receiver: MemorySegment, name: String, arg0: MemorySegment) {
-        objc_msgSend_nil_ptr.invokeExact(receiver, registerName(name), arg0)
+    // Handles for objc_msgSend():
+
+    private fun msgVoid(receiver: MemorySegment, name: String, arg: MemorySegment) {
+        msgSend_void_ptr.invokeExact(receiver, registerName(name), arg)
     }
 
-    private fun msgInt(receiver: MemorySegment, name: String) =
-        objc_msgSend_int.invokeExact(receiver, registerName(name)) as Int
+    private fun msgLong(receiver: MemorySegment, name: String): Long =
+        msgSend_long.invokeExact(receiver, registerName(name)) as Long
 
-    private fun msgPtr(receiver: MemorySegment, name: String) =
-        objc_msgSend_ptr.invokeExact(receiver, registerName(name)) as MemorySegment
+    private fun msgPtr(receiver: MemorySegment, name: String): MemorySegment =
+        notNull(msgSend_ptr.invokeExact(receiver, registerName(name)) as MemorySegment, name)
 
-    private fun msgPtr(receiver: MemorySegment, name: String, arg0: Int) =
-        objc_msgSend_ptr_int.invokeExact(receiver, registerName(name), arg0) as MemorySegment
+    private fun msgPtr(receiver: MemorySegment, name: String, arg: Long): MemorySegment =
+        notNull(msgSend_ptr_long.invokeExact(receiver, registerName(name), arg) as MemorySegment, "$name:$arg")
 
-    private fun msgPtr(receiver: MemorySegment, name: String, arg0: MemorySegment) =
-        objc_msgSend_ptr_ptr.invokeExact(receiver, registerName(name), arg0) as MemorySegment
+    private fun msgPtr(receiver: MemorySegment, name: String, arg: MemorySegment): MemorySegment =
+        notNull(msgSend_ptr_ptr.invokeExact(receiver, registerName(name), arg) as MemorySegment, "$name:$arg")
 
-    private val objc_msgSend_nil_ptr = objc_msgSend(null, ADDRESS)
-    private val objc_msgSend_int = objc_msgSend(JAVA_INT)
-    private val objc_msgSend_ptr = objc_msgSend(ADDRESS)
-    private val objc_msgSend_ptr_int = objc_msgSend(ADDRESS, JAVA_INT)
-    private val objc_msgSend_ptr_ptr = objc_msgSend(ADDRESS, ADDRESS)
+    private val msgSend_void_ptr = msgSendHandle(null, ADDRESS)
+    private val msgSend_long = msgSendHandle(JAVA_LONG)
+    private val msgSend_ptr = msgSendHandle(ADDRESS)
+    private val msgSend_ptr_long = msgSendHandle(ADDRESS, JAVA_LONG)
+    private val msgSend_ptr_ptr = msgSendHandle(ADDRESS, ADDRESS)
 
-    private fun objc_msgSend(resLayout: MemoryLayout?, vararg varargLayouts: MemoryLayout) =
+    private fun msgSendHandle(resLayout: MemoryLayout?, vararg varargLayouts: MemoryLayout) =
         Linker.nativeLinker().downcallHandle(
-            Linker.nativeLinker().defaultLookup().find("objc_msgSend").get(),
+            SymbolLookup.loaderLookup().find("objc_msgSend").get(),
             if (resLayout == null) FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, *varargLayouts)
-            else FunctionDescriptor.of(resLayout, ADDRESS, ADDRESS, *varargLayouts),
-            Linker.Option.firstVariadicArg(2)
+            else FunctionDescriptor.of(resLayout, ADDRESS, ADDRESS, *varargLayouts)
         )
 
 
-    private fun registerName(name: String) =
-        Arena.ofConfined().use { ar -> sel_registerName.invokeExact(ar.allocateUtf8String(name)) as MemorySegment }
+    // Handle for sel_registerName():
 
-    private val sel_registerName = Linker.nativeLinker().downcallHandle(
-        Linker.nativeLinker().defaultLookup().find("sel_registerName").get(),
+    private fun registerName(name: String): MemorySegment = Arena.ofConfined().use { arena ->
+        notNull(registerName.invokeExact(arena.allocateUtf8String(name)) as MemorySegment, "sel_regName($name)")
+    }
+
+    private val registerName = Linker.nativeLinker().downcallHandle(
+        SymbolLookup.loaderLookup().find("sel_registerName").get(),
         FunctionDescriptor.of(ADDRESS, ADDRESS)
     )
 
+
+    // Handles for dispatching onto the main thread:
+
+    private val dispatchMainQ = SymbolLookup.loaderLookup().find("_dispatch_main_q").get()
+
+    private val dispatchSyncF = Linker.nativeLinker().downcallHandle(
+        SymbolLookup.loaderLookup().find("dispatch_sync_f").get(),
+        FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS)
+    )
+
+    private val dispatchAsyncF = Linker.nativeLinker().downcallHandle(
+        SymbolLookup.loaderLookup().find("dispatch_async_f").get(),
+        FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, ADDRESS)
+    )
+
+
+    // Upcall stubs for methods in this class:
+
+    private val doInitialize = upcallStub("doInitialize")
+    private val doLocalize = upcallStub("doLocalize")
+
+    private fun upcallStub(name: String) = Linker.nativeLinker().upcallStub(
+        MethodHandles.dropArguments(
+            MethodHandles.lookup().bind(this, name, methodType(Void::class.javaPrimitiveType)),
+            0, MemorySegment::class.java
+        ),
+        FunctionDescriptor.ofVoid(ADDRESS),
+        Arena.global()
+    )
+
+
+    // The actual localization code:
 
     private class MenuItem(val l10nKey: String, val defaultTitleRegex: Regex) {
         var handle: MemorySegment? = null
@@ -97,7 +142,7 @@ class MacOSMenuLocalizer {
 
     private val menuItems = listOf(
         MenuItem("macMenu.about", Regex("About.*")),
-        MenuItem("macMenu.preferences", Regex("Preferences.*")),
+        MenuItem("macMenu.preferences", Regex("(Preferences|Settings).*")),
         MenuItem("macMenu.services", Regex("Services")),
         MenuItem("macMenu.hideApp", Regex("Hide [^O].*")),
         MenuItem("macMenu.hideOthers", Regex("Hide Others")),
@@ -107,27 +152,33 @@ class MacOSMenuLocalizer {
         MenuItem("macMenu.quit", Regex("Quit [^ ]*")),
     )
 
-
     init {
-        val appHandle = msgPtr(getClass("NSApplication"), "sharedApplication")
-        val appMenuHandle = msgPtr(msgPtr(msgPtr(appHandle, "mainMenu"), "itemAtIndex:", 0), "submenu")
-        for (i in 0..<msgInt(appMenuHandle, "numberOfItems").coerceAtMost(11)) {
+        dispatchSyncF.invokeExact(dispatchMainQ, NULL, doInitialize)
+    }
+
+    private fun doInitialize() {
+        val mainMenuHandle = msgPtr(msgPtr(getClass("NSApplication"), "sharedApplication"), "mainMenu")
+        check(msgLong(mainMenuHandle, "numberOfItems") > 0)
+        val appMenuHandle = msgPtr(msgPtr(mainMenuHandle, "itemAtIndex:", 0), "submenu")
+        for (i in 0..<msgLong(appMenuHandle, "numberOfItems")) {
             val itemHandle = msgPtr(appMenuHandle, "itemAtIndex:", i)
-            if (itemHandle == NULL)
-                break
             val title = msgPtr(msgPtr(itemHandle, "title"), "UTF8String").reinterpret(Long.MAX_VALUE).getUtf8String(0L)
             menuItems.find { it.defaultTitleRegex.matches(title) }?.handle = itemHandle
         }
     }
 
     private fun localize() {
+        dispatchAsyncF.invokeExact(dispatchMainQ, NULL, doLocalize)
+    }
+
+    private fun doLocalize() {
         for (menuItem in menuItems)
             menuItem.handle?.let { itemHandle ->
                 val title = l10n(menuItem.l10nKey)
                 val titleHandle = Arena.ofConfined().use { arena ->
                     msgPtr(getClass("NSString"), "stringWithUTF8String:", arena.allocateUtf8String(title))
                 }
-                msgNil(itemHandle, "setTitle:", titleHandle)
+                msgVoid(itemHandle, "setTitle:", titleHandle)
             }
     }
 
